a43f85113ed50c56f1ea6b9ccc14328267ffd67b
Guilherme Augusto <116207265+Guiggs67@users.noreply.github.com>
2023-05-22 17:23:36 -0300

    Add files via upload

---
+++ Atividade N2/Atividade N2/Armazenar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_N2
+{
+    internal class Armazenar
+    {
+        public string[] musica;
+        public string[] artista;
+        public string[] album;
+        public string[] conjunto;
+
+        public Armazenar(string musica, string artista, string album)
+        {
+            this.musica[100] = musica;
+            this.artista[100] = artista;
+            this.album[100] = album;
+        }
+
+        public string Conjunto(string musica, string artista, string album)
+        {
+            for (int i = 0; i < musica.Length; i++)
+            {
+              
+                
+
+
+            }
+        }
+
+
+
+    }
+
+
+
+}
---
+++ Atividade N2/Atividade N2/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade_N2
+{
+    public partial class Form1 : Form
+    {
+        string[] musica = new string[100];
+        string[] artista = new string[100];
+        string[] album = new string[100];
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < musica.Length; i++)
+            musica[i] = (string)this.textBox1.Text;
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < musica.Length; i++)
+            artista[i] = (string)this.textBox1.Text;
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < musica.Length; i++)
+            album[i] = (string)this.textBox1.Text;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < musica.Length; i++)
+            {
+                Armazenar P = new Armazenar(musica[i], artista[i], album[i]);
+                
+            }
+            
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < musica.Length; i++)
+            {
+                Armazenar P = new Armazenar(musica[i], artista[i], album[i]);
+                textBox4.Text = P.Conjunto();
+            }
+        }
+
+        
+    }
+}
